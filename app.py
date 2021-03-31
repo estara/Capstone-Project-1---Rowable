d940e437e8cc3f7abe16bdfd9fe59952e900b6a1
Estara <heatherjpdx@gmail.com>
2021-03-31 10:27:26 -0700

    fixed userdetail

--- app.py
+++ app.py
@@ -145,7 +145,10 @@ def user_details(user_id):
         db.session.add(user)
         db.session.commit()
         return redirect(f'/userdetail/{user_id}')
-    boathouses = [b for b in Boathouse.query.filter(Boathouse.id.in_(user.boathouses))]
+    if user.boathouses is not None:
+        boathouses = [b for b in Boathouse.query.filter(Boathouse.id.in_(user.boathouses))]
+    else:
+        boathouses = None
     return render_template('userdetail.html', form=form, user=user, boathouses=boathouses)
 
 
