cd15d13262228064fda723d1fd8053f1538118cd
Estara <heatherjpdx@gmail.com>
2021-04-01 14:41:25 -0700

    tests

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def user_details(user_id):
         db.session.commit()
         return redirect(f'/userdetail/{user_id}')
     boathouses = [b for b in Boathouse.query.filter(Boathouse.id.in_(user.boathouses))]
-    if not boathouses:
+    if boathouses == []:
         boathouses = None
     return render_template('userdetail.html', form=form, user=user, boathouses=boathouses)
 
